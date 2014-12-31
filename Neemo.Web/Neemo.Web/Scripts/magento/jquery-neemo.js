b65f7b931741c8a2b97cc58b3b426c1f13e2fcfd
Dejan Vasic <dejanvasic24@gmail.com>
2014-12-31 15:10:10 +1100

    Forcing numbers only on quantity input ctrl

---
+++ Neemo.Web/Neemo.Web/Scripts/magento/jquery-neemo.js
@@ -0,0 +1,12 @@
+﻿(function ($) {
+    $('input[data-numbers-only]').on('keypress', function (e) {
+        if ((e.which < 48 || e.which > 57)) {
+            if (e.which == 8 || e.which == 46 || e.which == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    });
+})(jQuery);
