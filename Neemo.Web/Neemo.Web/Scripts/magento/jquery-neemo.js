a562fe0b4128b247806a7ff7dbbafaa26cf21a73
Dejan Vasic <dejanvasic24@gmail.com>
2014-12-31 16:40:20 +1100

    Ajax call wired to add item to shopping cart

--- Neemo.Web/Neemo.Web/Scripts/magento/jquery-neemo.js
+++ Neemo.Web/Neemo.Web/Scripts/magento/jquery-neemo.js
@@ -1,4 +1,30 @@
-﻿(function ($) {
+﻿var neemojs = (function ($, toastr) {
+
+    // Cart functions
+    $('.btn-cart').on('click', function () {
+        var me = $(this);
+        var id = me.data().productid;
+        var qty = 1;
+
+        // Get the from the UI (if any)
+        var $qty = me.prev('input');
+        if ($qty) {
+            qty = $qty.val();
+        }
+
+        $.ajax({
+            url: me.data().carturl,
+            type: 'POST',
+            data: JSON.stringify({ productId: id, qty: qty }),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (response) {
+                toastr.info('Item has been added to your cart');
+            }
+        });
+    });
+
+    //jQuery hooks
     $('input[data-numbers-only]').on('keypress', function (e) {
         if ((e.which < 48 || e.which > 57)) {
             if (e.which == 8 || e.which == 46 || e.which == 0) {
@@ -9,4 +35,10 @@
             }
         }
     });
-})(jQuery);
+
+    // Service
+    function cartService() {
+        
+    }
+
+})(jQuery, toastr);
