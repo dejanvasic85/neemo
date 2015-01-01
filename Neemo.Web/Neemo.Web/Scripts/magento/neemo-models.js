e746ac25ffb94137ca8890d06ff9dfd26d788898
Dejan Vasic <dejanvasic24@gmail.com>
2015-01-01 17:04:41 +1100

    Adding partial view for editor

--- Neemo.Web/Neemo.Web/Scripts/magento/neemo-models.js
+++ Neemo.Web/Neemo.Web/Scripts/magento/neemo-models.js
@@ -1,7 +1,21 @@
 ﻿neemo = neemo || {};
 
 (function ($, accounting) {
-    
+
+    neemo.lineItem = function (cartViewItem) {
+        var me = this;
+
+        me.productId = ko.observable(cartViewItem.ProductId);
+        me.lineItemId = ko.observable(cartViewItem.LineItemId);
+        me.title = ko.observable(cartViewItem.Title);
+        me.price = ko.observable(cartViewItem.Price);
+        me.qty = ko.observable(cartViewItem.Quantity);
+
+        me.subTotal = function () {
+            return me.price() * me.qty;
+        }
+    };
+
     neemo.shoppingCart = function (items) {
         var me = this;
         me.items = ko.observableArray(items);
@@ -12,16 +26,15 @@
             });
             return accounting.formatMoney(total);
         };
-        me.totalQuantity = function() {
+        me.totalQuantity = function () {
             var total = 0;
             $.each(this.items(), function () {
                 total += this.Quantity;
             });
             return total;
         };
-        me.removeItem = function(item) {
+        me.removeItem = function (item) {
             me.items.remove(item);
-            debugger;
             neemo.svc.removeProduct(item.LineItemId);
         };
     };
