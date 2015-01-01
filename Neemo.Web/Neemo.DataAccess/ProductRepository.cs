7d7ea66d26ac1bac89f599b440cc0337cda077b4
Dejan Vasic <dejanvasic24@gmail.com>
2015-01-01 15:01:03 +1100

    Out of stock functionality on details page

--- Neemo.Web/Neemo.DataAccess/ProductRepository.cs
+++ Neemo.Web/Neemo.DataAccess/ProductRepository.cs
@@ -32,6 +32,20 @@ namespace Neemo.DataAccess
                 new Product{ ProductId = 4, ImageId="product-4", IsBestSeller = true, Title = "Another Allow", Price = 999, IsNew = true},
                 new Product{ ProductId = 5, ImageId="product-5", IsBestSeller = true, Title = "8 Spoke Allow", Price = 899, IsNew = true},
                 new Product{ ProductId = 6, ImageId="product-6", IsBestSeller = true, Title = "More Wheels man", Price = 499, IsNew = true},
+                new Product
+                {
+                    ProductId = 7, 
+                    ImageId="product-1",
+                    Images = new []{"product-1","product-2", "product-3"},
+                    IsBestSeller = true, 
+                    Title = "Product 7", 
+                    Price = 399, 
+                    IsFeatured = true,
+                    Description = "Purpose The foreach binding duplicates a section of markup for each entry in an array, and binds each copy of that markup to the corresponding array item. This is especially useful for rendering lists or tables.",
+                    QuickOverview = "Of course, you can arbitrarily nest any number of foreach bindings along with other control-flow ",
+                    Category = "Mercedes",
+                    AvailableQty = 0
+                }
             };
         }
     }
--- Neemo.Web/Neemo.Web/Controllers/ProductController.cs
+++ Neemo.Web/Neemo.Web/Controllers/ProductController.cs
@@ -15,8 +15,12 @@ namespace Neemo.Web.Controllers
 
         public ActionResult Details(int id)
         {
+            // Todo - this value should not be hardcoded
+            var productId = 7;
+            //var productId = id;
+
             // Fetch the full product details
-            var productView = Mapper.Map<Store.Product, Models.ProductDetailView>(_productService.GetProductById(1));
+            var productView = Mapper.Map<Store.Product, Models.ProductDetailView>(_productService.GetProductById(productId));
 
             return View(productView);
         }
