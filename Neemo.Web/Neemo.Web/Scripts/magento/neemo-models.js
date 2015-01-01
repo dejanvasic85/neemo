ad7963db0e6746fc7813e3d0cb2490ac6c5b683f
Dejan Vasic <dejanvasic24@gmail.com>
2015-01-01 13:50:11 +1100

    Shopping cart view implemented on master layout

--- Neemo.Web/Neemo.Web/Scripts/magento/neemo-models.js
+++ Neemo.Web/Neemo.Web/Scripts/magento/neemo-models.js
@@ -1,12 +1,29 @@
 ﻿neemo = neemo || {};
 
-neemo.lineItem = function (id, qty) {
-    var me = this;
-    me.id = ko.observable(id);
-    me.qty = ko.observable(qty);
-};
+(function ($, accounting) {
 
-neemo.shoppingCart = function (items) {
-    var me = this;
-    me.items = ko.observableArray(items);
-};
+    //neemo.lineItem = function(id, price, qty, title, imageId) {
+    //    var me = this;
+    //    me.id = ko.observable(id);
+    //    me.qty = ko.observable(qty);
+    //    me.title = ko.observable(title);
+    //    me.imageId = ko.observable(imageId);
+
+    //    me.subTotal = function() {
+            
+    //    }
+    //};
+
+    neemo.shoppingCart = function (items) {
+        var me = this;
+        me.items = ko.observableArray(items);
+        me.subTotal = function () {
+            var total = 0;
+            $.each(this.items(), function () {
+                total += (this.Price * this.Quantity);
+            });
+            return accounting.formatMoney(total);
+        };
+    };
+
+})(jQuery, accounting);
--- Neemo.Web/Neemo.Web/Scripts/magento/neemo-ui.js
+++ Neemo.Web/Neemo.Web/Scripts/magento/neemo-ui.js
@@ -39,9 +39,10 @@ neemo.ui = (function ($, toastr, svc, shoppingcart) {
         if ((e.which < 48 || e.which > 57)) {
             if (e.which === 8 || e.which === 46 || e.which === 0) {
                 return true;
+            } else {
+                return false;
             }
         }
-        return false;
     });
 
     // Initialise the shopping cart
