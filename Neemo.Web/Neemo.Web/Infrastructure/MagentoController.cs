aa2d278407e7c56170a6a88c037c6ae8e7e1dcf6
Dejan Vasic <dejanvasic24@gmail.com>
2015-01-11 16:48:27 +1100

    Logic for the checkout button

--- Neemo.Web/Neemo.Web/Infrastructure/MagentoController.cs
+++ Neemo.Web/Neemo.Web/Infrastructure/MagentoController.cs
@@ -4,12 +4,16 @@
     using Store;
     using System.Linq;
     using System.Web.Mvc;
+    using Neemo.ShoppingCart;
     using Microsoft.Practices.Unity;
 
     public class MagentoController : Controller
     {
         [Dependency]
         public ICategoryService CategoryService { get; set; }
+
+        [Dependency]
+        public ICartContext CartContext { get; set; }
         
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
@@ -18,6 +22,19 @@
                 .ToList();
 
             ViewBag.Categories = new Models.CategoryListView{AllCategories = categories};
+
+            if (CartContext.HasItemsInCart() && Request.IsAuthenticated)
+            {
+                ViewBag.CheckoutUrl = Url.Checkout();
+            }
+            else if (CartContext.HasItemsInCart() && !Request.IsAuthenticated)
+            {
+                ViewBag.CheckoutUrl = Url.Login();
+            }
+            else if (!CartContext.HasItemsInCart())
+            {
+                ViewBag.CheckoutUrl = Url.MyCart();
+            }
         }
     }
 }
