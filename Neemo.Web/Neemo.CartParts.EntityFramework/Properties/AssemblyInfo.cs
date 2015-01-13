ce0a580502509eb9c22fe0d5f359c3873a0c3169
Dejan Vasic <dejanvasic24@gmail.com>
2015-01-13 19:51:02 +1100

    Renaming the carts parts project

--- Neemo.Web/Neemo.CartParts.EntityFramework/Properties/AssemblyInfo.cs
+++ Neemo.Web/Neemo.CartParts.EntityFramework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Neemo.CartParts")]
+[assembly: AssemblyTitle("Neemo.CartParts.EntityFramework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Neemo.CartParts")]
+[assembly: AssemblyProduct("Neemo.CartParts.EntityFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3be7f543-3458-4277-97fe-7b103e3134f0")]
+[assembly: Guid("29ecd3c4-d7ea-4b1f-9b68-52cb244535d2")]
 
 // Version information for an assembly consists of the following four values:
 //
