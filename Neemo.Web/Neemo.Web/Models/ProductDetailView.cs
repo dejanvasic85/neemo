2d8ae4ced16528feea4871fb0001f2976e8579e2
Dejan Vasic <dejanvasic24@gmail.com>
2015-01-24 12:28:38 +1100

    Fixed minor defect for product when no images avail

--- Neemo.Web/Neemo.Web/Models/ProductDetailView.cs
+++ Neemo.Web/Neemo.Web/Models/ProductDetailView.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Neemo.Web.Models
 {
@@ -10,6 +11,17 @@ namespace Neemo.Web.Models
         public string QuickOverview { get; set; }
         public decimal Price { get; set; }
         public string[] Images { get; set; }
+
+        public string DefaultImage
+        {
+            get
+            {
+                return Images == null || Images.Length == 0
+                    ? "placeholder"
+                    : Images.First();
+            }
+        }
+
         public int AvailableQty { get; set; }
         public CategoryView Category { get; set; }
     }
