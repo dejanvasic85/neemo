e24e5f5cfb99223dec49a168ed6fd2cde135fc8b
Dejan Vasic <dejanvasic24@gmail.com>
2015-02-08 15:43:28 +1100

    Undoing bundling because it's not working as expect

--- Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
+++ Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace Neemo.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/neemo").Include("~/Scripts/magento/neemo*"));
+          
             //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
             //            "~/Scripts/jquery-{version}.js"));
 
