1c7df35c80634662001383818b8251a91b6cf0ff
Dejan Vasic <dejanvasic24@gmail.com>
2015-02-15 01:04:47 +1100

    More minification and bundling

--- Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
+++ Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Neemo.Web
 {
@@ -8,15 +7,14 @@ namespace Neemo.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //    bundles.Add(new Bundle("~/bundles/neemo")
-            //        .Include("~/Scripts/neemo-ko-extensions.js")
-            //        .Include("~/Scripts/neemo-models.js")
-            //        .Include("~/Scripts/neemo-svc.js")
-            //        .Include("~/Scripts/neemo-ui.js")
-            //        );
+            bundles.Add(new ScriptBundle("~/magento")
+                .Include("~/Scripts/magento/accounting.js")
+                .Include("~/Scripts/magento/toastr.js")
+                );
 
-            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").IncludeDirectory("~/Scripts/Magento", searchPattern: "bootstrap*", searchSubdirectories: false));
-            bundles.Add(new ScriptBundle("~/Scripts/neemo").IncludeDirectory("~/Scripts", searchPattern: "neemo-*", searchSubdirectories: false));
+            bundles.Add(new ScriptBundle("~/knockout").IncludeDirectory("~/Scripts", searchPattern: "knockout-*", searchSubdirectories: false));
+            bundles.Add(new ScriptBundle("~/bootstrap").IncludeDirectory("~/Scripts", searchPattern: "bootstrap*", searchSubdirectories: false));
+            bundles.Add(new ScriptBundle("~/neemo").IncludeDirectory("~/Scripts", searchPattern: "neemo-*", searchSubdirectories: false));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
