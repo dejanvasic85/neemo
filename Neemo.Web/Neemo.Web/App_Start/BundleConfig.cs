bd51d12f4b0d4f822963e5fef29f35b92160f74d
Dejan Vasic <dejanvasic24@gmail.com>
2015-02-15 00:26:49 +1100

    Bundling and minification for neemo and bootstrap

--- Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
+++ Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
@@ -8,21 +8,15 @@ namespace Neemo.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-          
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            //    bundles.Add(new Bundle("~/bundles/neemo")
+            //        .Include("~/Scripts/neemo-ko-extensions.js")
+            //        .Include("~/Scripts/neemo-models.js")
+            //        .Include("~/Scripts/neemo-svc.js")
+            //        .Include("~/Scripts/neemo-ui.js")
+            //        );
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
-
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").IncludeDirectory("~/Scripts/Magento", searchPattern: "bootstrap*", searchSubdirectories: false));
+            bundles.Add(new ScriptBundle("~/Scripts/neemo").IncludeDirectory("~/Scripts", searchPattern: "neemo-*", searchSubdirectories: false));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
