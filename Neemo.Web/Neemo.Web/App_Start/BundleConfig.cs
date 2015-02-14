1b788cb5938ab7247f7e8568c2ea77d5a72f4da6
Dejan Vasic <dejanvasic24@gmail.com>
2015-02-15 01:44:23 +1100

    Minification of scripts complete

--- Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
+++ Neemo.Web/Neemo.Web/App_Start/BundleConfig.cs
@@ -7,7 +7,13 @@ namespace Neemo.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/jquery").IncludeDirectory("~/Scripts", "jquery*"));
+
             bundles.Add(new ScriptBundle("~/magento")
+                .Include("~/Scripts/magento/prototype.js")
+                .Include("~/Scripts/magento/ccard.js")
+                .Include("~/Scripts/magento/validation.js")
+                .Include("~/Scripts/magento/builder.js")
                 .Include("~/Scripts/magento/effects.js")
                 .Include("~/Scripts/magento/dragdrop.js")
                 .Include("~/Scripts/magento/controls.js")
@@ -20,8 +26,6 @@ namespace Neemo.Web
                 .Include("~/Scripts/magento/ma.jq.slide.js")
                 .Include("~/Scripts/magento/ma.mobilemenu.js")
                 .Include("~/Scripts/magento/ajax_cart_super.js")
-                .Include("~/Scripts/magento/jquery.fancybox.js")
-                .Include("~/Scripts/magento/jquery-ui.js")
                 .Include("~/Scripts/magento/ma.dropdown.js")
                 .Include("~/Scripts/magento/ma.accordion.js")
                 .Include("~/Scripts/magento/ma.flexslider.js")
