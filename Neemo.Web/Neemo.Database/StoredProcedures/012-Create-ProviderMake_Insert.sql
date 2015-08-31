fa4acebab2cdff99825dfcb39ff926b9290e795b
wijayakoon <janaka@nettonetsolutions.com>
2015-08-31 23:45:29 +1000

    Db Scripts for Makes Implementation for Provider

---
+++ Neemo.Web/Neemo.Database/StoredProcedures/012-Create-ProviderMake_Insert.sql
@@ -0,0 +1,20 @@
+﻿
+IF OBJECT_ID('[dbo].[sp_getProviderMakesr]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[sp_getProviderMakes] 
+END 
+GO
+CREATE  Procedure [dbo].[sp_getProviderMakes]
+	@ServiceTypeID  int
+	,@ServiceType  nvarchar(100)
+	,@ProviderID  int
+AS
+select PST.ServiceTypeID,ST.ServiceType ,PST.Active from ProviderServiceType PST 
+inner join  ServiceType ST on PST.ServiceTypeID = ST.ServiceTypeID
+where (PST.ProviderID = @ProviderID  OR @ProviderID  =0)
+AND (PST.ServiceTypeID= @ServiceTypeID  OR @ServiceTypeID  =0)
+AND (ST.ServiceType =@ServiceType  OR @ServiceType='')
+AND PST.Active = 1
+GO
+
+GO
