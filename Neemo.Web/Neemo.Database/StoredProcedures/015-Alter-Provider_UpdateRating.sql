f1728c148481d0d25ac32c8b44b7a97ff4c373ca
Dejan Vasic <dejanvasic24@gmail.com>
2015-09-25 23:55:45 +1000

    Fixing the update rating stored procedure

---
+++ Neemo.Web/Neemo.Database/StoredProcedures/015-Alter-Provider_UpdateRating.sql
@@ -0,0 +1,27 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (
+	SELECT * 
+	FROM sys.objects 
+	WHERE object_id = OBJECT_ID(N'[dbo].[Provider_UpdateRating]') 
+		AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'
+
+CREATE PROCEDURE [dbo].[Provider_UpdateRating]
+       @ProviderId	INT,
+	   @Rating		DECIMAL (18,2),
+	   @User		VARCHAR (100)
+
+AS
+BEGIN
+	UPDATE Provider
+	SET		Rating = @Rating,
+			LastModifiedByUser = @User
+	WHERE	ProviderID = @ProviderId
+END
+' 
+END
